87d893eb4d6d7162d252358bc31392add8881f21
doutoury <doutoury@naver.com>
2021-04-15 17:52:02 +0900

    after deployment, add build folder

---
+++ build/precache-manifest.91755fbf45693e34d999dee423086abe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae5f8e8875dbd433f8a1f7acd4a039e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e005e4861e1dda6bcb3",
+    "url": "/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "43f2f3423ee85dcbf118",
+    "url": "/static/css/main.710f580f.chunk.css"
+  },
+  {
+    "revision": "7e005e4861e1dda6bcb3",
+    "url": "/static/js/2.f1a1052a.chunk.js"
+  },
+  {
+    "revision": "43f2f3423ee85dcbf118",
+    "url": "/static/js/main.28b71fdb.chunk.js"
+  },
+  {
+    "revision": "205082ab9e90481d5e69",
+    "url": "/static/js/runtime-main.ed6f88de.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
